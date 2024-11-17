5a89e9603c007dc0539801a18b30d1beaaf90cab
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-11-17 15:32:55 +0000

    #1 done demo

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -4,13 +4,14 @@ from os.path import dirname, realpath, join
 from arekit.common.data.input.providers.const import IDLE_MODE
 from arekit.common.pipeline.batching import BatchingPipelineLauncher
 from arekit.common.pipeline.context import PipelineContext
+from arekit.contrib.utils.pipelines.items.text.entities_default import TextEntitiesParser
 
 from bulk_translate.src.pipeline.translator import MLTextTranslatorPipelineItem
 from bulk_translate.src.service_dynamic import dynamic_init
 
 
 class TestTranslatorPipeline(unittest.TestCase):
-    text = "C'était en juillet 1805, et l'oratrice était la célèbre Anna Pavlovna"
+    text = "C'était en [juillet_1805] , et l'oratrice était la célèbre [Anna_Pavlovna]"
 
     CURRENT_DIR = dirname(realpath(__file__))
 
@@ -20,11 +21,12 @@ class TestTranslatorPipeline(unittest.TestCase):
                                          class_name="GoogleTranslateModel")()
 
         pipeline = [
+            TextEntitiesParser(src_func=lambda text: text.split()),
             MLTextTranslatorPipelineItem(batch_translate_model=translation_model.get_func(src="auto", dest="en"),
                                          do_translate_entity=False),
         ]
 
-        ctx = PipelineContext(d={"input": [[TestTranslatorPipeline.text]]},
+        ctx = PipelineContext(d={"input": [TestTranslatorPipeline.text]},
                               parent_ctx=PipelineContext({IDLE_MODE: False}))
 
         BatchingPipelineLauncher.run(pipeline=pipeline, pipeline_ctx=ctx, src_key="input")
